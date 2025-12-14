c5f2db99e1aa10db157d389070fc0b1969d71e09
akramikram24-web <akramikram24@gmail.com>
2025-12-14 12:28:15 +0800

    Update ringTexts descriptions in script.js

--- script.js
+++ script.js
@@ -588,8 +588,8 @@ scene.add(planet);
 //Ganti Deskripsi Planet
 const ringTexts = [
   'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
-  "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
+  "I love you kiki",//untuk deskripsi planet layer 2
+  "i love you kiki",//untuk deskripsi planet layer 3
   "01/08/2025",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
